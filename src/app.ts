cdac290b4b10c2ae16bdd0d49576789bb00b80b9
Doreen162 <doreenligman17@gmail.com>
2021-04-09 11:51:09 +0200

    Modules

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-// classes
-class Invoice {
- // readonly client: string;
- // private details: string;
-//  public amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number,
-  ){}
-
-  format(){
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250)
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
@@ -28,8 +13,10 @@ invoices.forEach(inv =>{
   console.log(inv.client, inv.amount, inv.format());
 });
 
+
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
 
 
 // inputs
