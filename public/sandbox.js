5ceca266518b5cde3d1b2228123b5380b991f3e0
Doreen162 <doreenligman17@gmail.com>
2021-04-09 09:04:51 +0200

    Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('mario');
+var greet;
+//greet = 'hello';
+greet = function () {
+    console.log('hello, again');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+// or '20'
+add(5, 10, 20);
