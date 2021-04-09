7c84c5177a7d8d1e3778f54e33af71c633f49cb6
Doreen162 <doreenligman17@gmail.com>
2021-04-09 15:33:07 +0200

    Generics

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { HasFormatter } from './interfaces/Hasformatter.js';
-import { ListTemplate } from './classes/ListTemplate.js'
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
@@ -11,6 +12,7 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
 const ul = document.querySelector('ul')!;
 const list = new ListTemplate(ul);
 
@@ -26,3 +28,46 @@ form.addEventListener('submit', (e: Event) => {
   
   list.render(doc, type.value, 'end');
 });
+
+// GENERICS
+
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+const addUID = <T extends {name: string}>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 40});
+//let docTwo = addUID('shaun');
+
+console.log(docOne.name);
+
+// with interfaces
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1, 
+  resourceName: 'person', 
+  data: { name: 'shaun' }
+};
+
+const docFour: Resource<string[]> = {
+  uid: 1, 
+  resourceName: 'shoppingList', 
+  data: ['bread', 'milk']
+};
+
+console.log(docThree, docFour);
