59a37c7adee897d6d025c4ed6d185398c074dd4a
Doreen162 <doreenligman17@gmail.com>
2021-04-09 13:11:22 +0200

    Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,37 @@
 import { Invoice } from './classes/Invoice.js';
 
-const invOne = new Invoice('mario', 'work on the mario website', 250)
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300)
-//console.log(invOne, invTwo );
+// interfaces
+export interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
 
+const me: IsPerson = {
+  name: 'shaun',
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+console.log(me);
+me.speak('hello, world');
 
-invoices.forEach(inv =>{
-  console.log(inv.client, inv.amount, inv.format());
-});
+const greetPerson = (person: IsPerson): void => {
+  console.log('hello ', person.name);
+}
 
+greetPerson(me);
+//greetPerson({name: 'shaun'});
 
-//const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
-
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
