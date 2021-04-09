4a0bfd7223ac078d1de40f72035f3ae2f24ad281
Doreen162 <doreenligman17@gmail.com>
2021-04-09 10:36:44 +0200

    Classes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,28 @@
 "use strict";
-var anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
-}
-console.log(anchor.href);
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes " + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+//console.log(invOne, invTwo );
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invOne, invTwo);
+console.log(invoices);
 //const form = document.querySelector('form')!;
 var form = document.querySelector('.new-item-form');
-console.log(form.children);
 // inputs
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
